11967f512e18e9910fa4b8412fbcf4f8d5b33562
Collin Morudi <collinmorudi@gmail.com>
2025-09-15 15:14:58 +0200

    Feat: Added a function to render html content

--- twimba/index.js
+++ twimba/index.js
@@ -36,7 +36,7 @@ function getFeedHtml(){
 return feedHtml
 /*
 Challenge:
-1. Use a "for of" to iterate over the data and 
+1. Use a "for of" to iterate over the data and
    create HTML string for each tweet using the 
    boilerplate below. Replace UPPERCASE text
    with data from the tweets. 
@@ -46,31 +46,9 @@ Challenge:
 */  
 }
 
-getFeedHtml()
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
 
-
-
-/*
-<div class="tweet">
-    <div class="tweet-inner">
-        <img src="URL OF PROFILE PIC" class="profile-pic">
-        <div>
-            <p class="handle">TWEET HANDLE</p>
-            <p class="tweet-text">TWEET TEXT</p>
-            <div class="tweet-details">
-                <span class="tweet-detail">
-                    NUMBER OF REPLIES
-                </span>
-                <span class="tweet-detail">
-                    NUMBER OF LIKES
-                </span>
-                <span class="tweet-detail">
-                    NUMBER OF RETWEETS
-                </span>
-            </div>   
-        </div>            
-    </div>
-</div>
-
-*/
+render()
 
