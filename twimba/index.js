4e34cb96bd11eae90e7e5e6d54a48c1c18627c35
Collin Morudi <collinmorudi@gmail.com>
2025-09-29 12:55:45 +0200

    Feature: added retweet logic

--- twimba/index.js
+++ twimba/index.js
@@ -10,6 +10,15 @@ document.addEventListener('click', function(e){
     if(e.target.dataset.like){
        handleLikeClick(e.target.dataset.like) 
     }
+    else if(e.target.dataset.retweet){
+        handleRetweetClick(e.target.dataset.retweet)
+    }
+/*
+Challenge:
+1. Make this eventListener call "handleRetweetClick" 
+   when the retweet icon is clicked, passing in the
+   uuid from that tweet.  
+*/
 })
 
 function handleLikeClick(tweetId){ 
@@ -17,13 +26,38 @@ function handleLikeClick(tweetId){
         return tweet.uuid === tweetId
     })[0]
 
-    if (targetTweetObj.isLiked) {
+    if (targetTweetObj.isLiked){
         targetTweetObj.likes--
-    } else {
-        targetTweetObj.likes++
+    }
+    else{
+        targetTweetObj.likes++ 
     }
     targetTweetObj.isLiked = !targetTweetObj.isLiked
     render()
+}
+
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+    
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }
+    else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    render()
+
+/*
+Challenge:
+2. Find the retweeted tweet's object in tweetsData 
+   and save it to a const.
+3. Increment or decrement the retweet count of the 
+   tweet and flip its isRetweeted boolean.
+4. Call the render function.  
+*/   
 }
 
 function getFeedHtml(){
