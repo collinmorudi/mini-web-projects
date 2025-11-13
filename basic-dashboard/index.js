647bed254770e4c74ff2996617ef9a53bb67e80a
Collin Morudi <collinmorudi@gmail.com>
2025-11-13 20:34:23 +0200

    feat: add starter code

--- basic-dashboard/index.js
+++ basic-dashboard/index.js
@@ -1,18 +1,32 @@
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+		document.getElementById("author").textContent = `By: ${data.user.name}`
+    })
+    .catch(err => {
+        // Use a default background image/author
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+		document.getElementById("author").textContent = `By: Dodi Achmad`
+    })
+
 /**
- * Challenge: get a random image from Unsplash and set it as the background
+ * Challenge: Pull down the cryptocurrency data for dogecoin from the 
+ * CoinGecko API and log it to the console
  * 
- * Part 1:
- * 
- * URL: https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
- * (You can change the "query" at the end to whatever theme you want)
- * 
- * Change the body's backgroundImage to: 
- * `url(<insert the URL of the iamge from the API here>)`
- * 
- * (You may need to dig around the response body a bit to find this URL)
- * 
- * (Note I've already added some CSS to resize the image within the window.
- * Instructions for this were found on CSS Tricks: 
- * https://css-tricks.com/perfect-full-page-background-image/#awesome-easy-progressive-css3-way)
+ * Also add a .catch() method to console any errors that might occur to the console
  */
 
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        console.log(res.status)
+        return res.json()
+    })
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => console.error(err))
