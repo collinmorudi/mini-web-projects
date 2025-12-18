874b993f6a3459550f253ab2833cc9902a4e7607
Collin Morudi <collinmorudi@gmail.com>
2025-12-18 16:59:22 +0200

    feat: add utils modules

--- from-the-other-side/server.js
+++ from-the-other-side/server.js
@@ -1,10 +1,16 @@
-/*
-Challenge: 
-1. Initialise a nodejs project:
-	Name: “from-the-other-side”.
-    Description: “A platform for sharing ghostly encounters”.
+import http from 'node:http'
+import { serveStatic } from './utils/serveStatic.js'
 
-2. Enable modular js (in package.json).
+const PORT = 8000
 
-hint.md for help
-*/
+const __dirname = import.meta.dirname
+
+/* Make any changes needed to the code below. */
+const server = http.createServer((req, res) => {
+    serveStatic(__dirname)
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<html><h1>The server is working</h1></html>')
+})
+
+server.listen(PORT, ()=> console.log(`Connected on port: ${PORT}`))
